36a46347f488dfbefa808061a70be10f12044597
Sai Vanam <bharatvanam.34@gmail.com>
2025-07-23 09:48:26 -0400

    Updated health message in main branch

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/health')
 def health():
-    return jsonify({"status": "UP"})
+    return jsonify({"status": "SYSTEM HEALTH OK"})
 
 @app.route('/metrics')
 def metrics():
